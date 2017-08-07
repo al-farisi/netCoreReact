be12fee96fba4556b6504e3856ebc5fd1c7f0476
M Salman Al Farisi <MuhammadSalman.AlFarisi@mitrais.com>
2017-08-07 09:14:45 +0700

    Add entity to database

--- NetCoreReact/src/NetCoreReact/Data/Migrations/20170807021209_AddedProjectEntity.Designer.cs
+++ NetCoreReact/src/NetCoreReact/Data/Migrations/20170807021209_AddedProjectEntity.Designer.cs
@@ -8,9 +8,10 @@ using NetCoreReact.Data;
 namespace NetCoreReact.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170807021209_AddedProjectEntity")]
+    partial class AddedProjectEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
---
+++ NetCoreReact/src/NetCoreReact/Data/Migrations/20170807021209_AddedProjectEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace NetCoreReact.Data.Migrations
+{
+    public partial class AddedProjectEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
